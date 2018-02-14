c4d6c0db7258ff5d84a0467d2e82e82f040e8354
Deirdre Moran <morand@os1.engr.oregonstate.edu>
2018-02-14 13:51:53 -0800

    Added more modularization

--- Adventure/morand.buildrooms.c
+++ Adventure/morand.buildrooms.c
@@ -1,16 +1,38 @@
+/***********************************************************************
+*
+*  Author: Deirdre Moran
+*  Program: morand.buildrooms.c
+*  Date: 2/13/2018
+*  Description: Builds room files for adventure game incorporating
+*				file reading and writing, threading, and mutexes.
+*
+************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <stdbool.h>
-//#include <fcnt1.h>
 
-
+// Max rooms connections
 #define MAX_CONNECTIONS 6
+// Max number of selected rooms in game
 #define MAX_ROOMS 7
-#define INITIAL_ROOM_TYPES 3
+// Number of room types
+#define NUM_ROOM_TYPES 3
+// Number of total rooms
 #define TOTAL_ROOMS 10
 
+
+/***********************************************************************
+*  Structure:			    room
+*  Description:  			Information for graph of rooms
+*		name:  				name of room
+*		numConnections: 	number of rooms connected to this room
+*		count:				count of
+*		roomID:				room ID for non-string room identification
+*		roomType:  			type of room (START_ROOM, MID_ROOM, END_ROOM)
+*		connectedRooms:		array of pointers to connected room structures
+************************************************************************/
 struct room
 {
 	char * name;
@@ -21,32 +43,47 @@ struct room
 	int connectedRooms[6];
 } myRooms[7];
 
+
+/***********************************************************************
+*  Function: 			initializeRooms()
+*  Description:			Sets myRoom struct to 0/NULL
+*  Parameters:   		None
+*  Pre-conditions:		myRoom struct memory allocated
+*  Post-conditions:		None
+*  Return:				None
+************************************************************************/
 void initializeRooms(){
 	int i, j, k;
 	for (i = 0; i < MAX_ROOMS; i++){
-			myRooms[i].name = calloc(16, sizeof(char));
-			strcpy(myRooms[i].name, " ");
-			myRooms[i].numConnections = 0;
-			myRooms[i].count = 0;
-			for (j = 0; j < MAX_CONNECTIONS; j++){
-				myRooms[i].connectedRooms[j] = 99;
-			}
+		myRooms[i].name = calloc(16, sizeof(char));
+		strcpy(myRooms[i].name, " ");
+		myRooms[i].numConnections = 0;
+		myRooms[i].count = 0;
+		for (j = 0; j < MAX_CONNECTIONS; j++){
+			myRooms[i].connectedRooms[j] = 99;
 		}
-
-
-		k = 0;
-		for (j = 40; j < 47; j++){
-			myRooms[k].roomID = j;
-			k++;
+	}
+	k = 0;
+	for (j = 40; j < 47; j++){
+		myRooms[k].roomID = j;
+		k++;
 	}
 }
 
 
+/***********************************************************************
+*  Function: 			randomNumberGenerator()
+*  Description:			generates array of random numbers
+*  Parameters:   		number of random numbers to generate
+*  Pre-conditions:		size of array to generate
+*  Post-conditions:		None
+*  Return:				array of random numbers
+************************************************************************/
 int * randomNumberGenerator(int size){
-		int i, j, k;
-
+	int i, j;
 	int ranlist[size];
 	int ranNum;
+	// create unique array of random numbers in specified size range
 	ranNum = rand() % size;
 	for (i = 0; i < size; i++){
 		ranlist[i] = size + 1;
@@ -77,177 +114,210 @@ int * randomNumberGenerator(int size){
 }
 
 
-
-
-
-
-static void assignRandomRoomNames(char **rmNames){
-		int i, j, k;
-
+/***********************************************************************
+*  Function: 			assignRandomRoomNames()
+*  Description:			Generates array of unique random numbers to
+*						assign random room names
+*  Parameters:   		Pointers to hardcoded room names
+*  Pre-conditions:		hardcoded room names created
+*  Post-conditions:		None
+*  Return:				None
+************************************************************************/
+static void assignRandomRoomNames(char **rmNames)
+{
+	int i, j, k;
 	int * ranNumList;
 	ranNumList = randomNumberGenerator(10);
-	// Assign random room names to 7 rooms
+	// Assign random room names to # of rooms
 	for(i = 0; i < MAX_ROOMS; i++){
 		myRooms[i].name = rmNames[ranNumList[i]];
 	}
 }
 
-static void assignRandomRoomTypes(char ** rmTypes){
-		int i, j, k;
 
+/***********************************************************************
+*  Function: 			assignRandomRoomTypes()
+*  Description:			Generate an array of unique random numbers
+*						and assign random room types
+*  Parameters:   		Pointers to hardcoded room types
+*  Pre-conditions:		myRoom struct memory set aside
+*  Post-conditions:		None
+*  Return:				None
+************************************************************************/
+static void assignRandomRoomTypes(char ** rmTypes)
+{
+	int i, j, k;
 	// Generate another array of unique random numbers to assign random room types
 	int * ranNumList2;
 	ranNumList2 = randomNumberGenerator(3);
 	// Assign random room for first 3 rooms (guarentees 1 start room, 1 end room)
 	for(j = 0; j < MAX_ROOMS; j++){
 		//Assign random rooms to first 3 random structs
-		if (j < INITIAL_ROOM_TYPES){
+		if (j < NUM_ROOM_TYPES){
 			myRooms[j].roomType = rmTypes[ranNumList2[j]];
 		}
 		//Assign midroom roomtype to remaining rooms
-		if (j >= INITIAL_ROOM_TYPES){
+		if (j >= NUM_ROOM_TYPES){
 			myRooms[j].roomType = rmTypes[2];
 		}
 	}
 }
 
+/***********************************************************************
+*  Function: 			assignRandomNumRoomConnections()
+*  Description:			Assigns random number of room connections
+*  Parameters:   		None
+*  Pre-conditions:		myRoom struct memory set aside
+*  Post-conditions:		None
+*  Return:				None
+************************************************************************/
 void assignRandomNumRoomConnections(){
 	// Generate list of random numbers for # room connections
-		int i, j, k, r;
-
-		srand((unsigned)time(NULL));
-		int peter[7];
-		int * ranNumList4;
-		for (k = 0; k < MAX_ROOMS; k++){
-			int r = rand() % 6 + 1;
-		 	do
-				r = rand() % 6 + 1;
-			while (r < INITIAL_ROOM_TYPES);
-			peter[k] = r;
-			// Assign random number of connections to a each room
-			myRooms[k].numConnections = r;
-	}
-
-}
-
-
-
-void assignRandomRoomConnections(){
-	int i, j;
-	int randomN;
-	int w;
+	int i, r;
+	srand((unsigned)time(NULL));
 	for (i = 0; i < MAX_ROOMS; i++){
-		j = 0;
-			for(j = 0; j < MAX_CONNECTIONS; j++){
-				// Generate random number between 0 and 7
-				randomN = (rand() % (7));
+		int r = rand() % 6 + 1;
+	 	do
+			r = rand() % 6 + 1;
+		while (r < NUM_ROOM_TYPES);
 
-				if(randomN == i && i != 6)
-				{
-					randomN++;
-				}
-				if (randomN == i && i == 6){
-					randomN--;
-				}
+		// Assign random number of connections to a each room
+		myRooms[i].numConnections = r;
+	}
+}
 
-					myRooms[i].connectedRooms[myRooms[i].count] = myRooms[randomN].roomID;
-					myRooms[i].count++;
-					for (w = 0; w < j; w++){
-						if(myRooms[i].connectedRooms[w] == myRooms[randomN].roomID)
-						{
-							myRooms[i].count--;
-							w = j - 1;
-							j--;
-						}
-					}
+
+/***********************************************************************
+*  Function: 			assignRandomRoomConnections()
+*  Description:			Assigns random room connections
+*  Parameters:   		None
+*  Pre-conditions:		myRoom struct memory set aside
+*  Post-conditions:		None
+*  Return:				None
+************************************************************************/
+void assignRandomRoomConnections()
+{
+	int i, j, k;
+	int randomN;
+	//for each room
+	for (i = 0; i < MAX_ROOMS; i++){
+		for(j = 0; j < MAX_CONNECTIONS; j++){
+			// Generate random number between 0 and 7
+			randomN = (rand() % (7));
+
+			if(randomN == i && i != 6)
+			{
+				randomN++;
 			}
-
+			if (randomN == i && i == 6){
+				randomN--;
+			}
+			// assign random room as connected room
+			myRooms[i].connectedRooms[myRooms[i].count] = myRooms[randomN].roomID;
+			myRooms[i].count++;
+			for (k = 0; k < j; k++){
+				if(myRooms[i].connectedRooms[k] == myRooms[randomN].roomID)
+				{
+					myRooms[i].count--;
+					k = j - 1;
+					j--;
+				}
+			}
+		}
+	}
 }
-}
 
-
-
+/***********************************************************************
+*  Function: 			createRoomFiles()
+*  Description:			Creates room files and writes info to for game play
+*  Parameters:   		pointers to hardcoded file names
+*  Pre-conditions:		myRooms struct memory set aside
+*  Post-conditions:		None
+*  Return:				None
+************************************************************************/
 void createRoomFiles(char ** flNames){
 	int i, j, k;
+	// file pointer
 	FILE * myFile;
-		char fileBuffer[50];
-		int pid = getpid();
-		sprintf(fileBuffer, "morand.rooms.%d", pid);
-		mkdir (fileBuffer, 0755);
-		char fileBuffer2[50];
-		for (i = 0; i < MAX_ROOMS; i++){
-			snprintf(fileBuffer2, sizeof(fileBuffer2), "morand.rooms.%d/%s", pid, flNames[i]);
-			myFile = fopen(fileBuffer2, "w");
-			fprintf(myFile, "ROOM NAME: %s\n", myRooms[i].name);
-
-			for (j = 0; j < myRooms[i].numConnections; j++){
-				fprintf(myFile, "CONNECTION %d: ", j + 1);
-					for(k = 0; k < MAX_ROOMS; k++){
-						if(myRooms[k].roomID == myRooms[i].connectedRooms[j]){
-							fprintf(myFile, "%s\n", myRooms[k].name);
-						}
-					}
-
+	char fileBuffer[50];
+	// get process id
+	int pid = getpid();
+	// create directory name
+	sprintf(fileBuffer, "morand.rooms.%d", pid);
+	// create directory
+	mkdir (fileBuffer, 0755);
+	char fileBuffer2[50];
+	// create seven files from f1Names file names
+	for (i = 0; i < MAX_ROOMS; i++){
+		snprintf(fileBuffer2, sizeof(fileBuffer2), "morand.rooms.%d/%s", pid, flNames[i]);
+		// open file and write contents of myRoom to it
+		myFile = fopen(fileBuffer2, "w");
+		fprintf(myFile, "ROOM NAME: %s\n", myRooms[i].name);
+		for (j = 0; j < myRooms[i].numConnections; j++){
+			fprintf(myFile, "CONNECTION %d: ", j + 1);
+			for(k = 0; k < MAX_ROOMS; k++){
+				if(myRooms[k].roomID == myRooms[i].connectedRooms[j]){
+					fprintf(myFile, "%s\n", myRooms[k].name);
+				}
 			}
-			fprintf(myFile, "ROOM TYPE: %s\n", myRooms[i].roomType);
 		}
+		fprintf(myFile, "ROOM TYPE: %s\n", myRooms[i].roomType);
+	}
 	fclose(myFile);
 }
 
 
 
 
-
-
-
-
-int main(){
-
-		srand((unsigned)time(NULL));
+/************************************************************************
+*************************************************************************
+*************************************************************************
+*************************************************************************
+***********													  ***********
+****						  MAIN() 								  ***
+***********													  ***********
+*************************************************************************
+*************************************************************************
+*************************************************************************
+************************************************************************/
+int main()
+{
+	// Seed random number
+	srand((unsigned)time(NULL));
 	int ranNum;
 	int i, j, k;
 
 	// Array of hardcoded file names
 	char * fileNames[7];
 	char fNames[7][100] = { "file1", "file2", "file3", "file4", "file5", "file6", "file7" };
+	// Array of 3 ptrs to chars to hold roomtype string
+	char * roomTypes[3];
 	// Array of hardcoded roomTypes
 	char types[3][11] = { "START_ROOM", "END_ROOM", "MID_ROOM" };
-	// Array of 3 ptrs to chars
-	char * roomTypes[3];
-	char buffer[100];
-	// Array of hardcoded room names
+	// Pointers to room names and array of hardcoded room names
 	char * roomNames[10];
 	char names[10][100] = {"Attic", "Basement", "Kitchen", "Loft", "Bedroom", "Den", "Bathroom", "Study", "Closet", "Stable" };
-	// Fill array of ptrs to point to hardcoded room names
+	// Fill array of ptrs to point to hardcoded room names, room types, and file names
 	for (i = 0; i < TOTAL_ROOMS; i++)
 	{
 		roomNames[i] = names[i];
 	}
-	for (i = 0; i < INITIAL_ROOM_TYPES; i++){
+	for (i = 0; i < NUM_ROOM_TYPES; i++){
 		roomTypes[i] = types[i];
 	}
 	for (i = 0; i < MAX_ROOMS; i++){
 			fileNames[i] = fNames[i];
 	}
-
 	//Initialize rooms
 	initializeRooms();
-
-	// Generate an array of unique random numbers to assign random room names
+	// Generate an array of unique random numbers and assign random room names
 	assignRandomRoomNames(roomNames);
-
-		// Generate another array of unique random numbers to assign random room types
-
+	// Generate an array of unique random numbers and assign random room types
 	assignRandomRoomTypes(roomTypes);
-	// ?????????room ids ??
-// Assign random number of room connections
+	// Assign random number of room connections
 	assignRandomNumRoomConnections();
-
-//------------------------------------------------------------------------------------
-assignRandomRoomConnections();
-
-createRoomFiles(fileNames);
-
-
+	// Assign random room connections
+	assignRandomRoomConnections();
+	// Create room files for game play
+	createRoomFiles(fileNames);
 }
-
